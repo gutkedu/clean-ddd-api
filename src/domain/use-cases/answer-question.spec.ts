9bb704736a5b4dcb796f7c518427550272fb10f8
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-31 22:55:50 -0300

    feat: external dependencies

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { test, expect } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.execute({
     instructorId: "any_instructor_id",
     questionId: "any_question_id",
     content: "any_content",
