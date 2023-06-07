d324525699cbf90092fcf3155d6e260ab06d4240
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-06-07 19:13:03 -0300

    feat: add eslint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,19 @@
-import { Answer } from "@/domain/entities/answer";
-import { AnswersRepository } from "@/domain/repositories/answers-repository";
-import { AnswerQuestionUseCase } from "@/domain/use-cases/answer-question";
+import { Answer } from '@/domain/entities/answer'
+import { AnswersRepository } from '@/domain/repositories/answers-repository'
+import { AnswerQuestionUseCase } from '@/domain/use-cases/answer-question'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
-};
+  create: async (answer: Answer) => {},
+}
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
   const answer = await answerQuestion.execute({
-    instructorId: "any_instructor_id",
-    questionId: "any_question_id",
-    content: "any_content",
-  });
+    instructorId: 'any_instructor_id',
+    questionId: 'any_question_id',
+    content: 'any_content',
+  })
 
-  expect(answer.content).toEqual("any_content");
-});
+  expect(answer.content).toEqual('any_content')
+})
