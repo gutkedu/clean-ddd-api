40cd5cbd14d8aa17ab1ad23ce414e1f4b824a51e
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-08-09 22:26:38 -0300

    feat: fetch answer comments

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -24,5 +25,15 @@ export class InMemoryAnswerCommentsRepository
     })
 
     return item ?? null
+  }
+
+  async findManyByAnswerId(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+    return answerComments
   }
 }
